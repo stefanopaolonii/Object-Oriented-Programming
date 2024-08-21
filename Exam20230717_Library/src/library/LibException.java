488e806b5b606ec97e45369ecb298eb3d5f60647
Stefano <s283110@studenti.polito.it>
2024-08-21 14:45:52 +0200

    Initial Commit

---
+++ Exam20230717_Library/src/library/LibException.java
@@ -0,0 +1,12 @@
+package library;
+
+public class LibException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public LibException() {super("Library exception");}
+	
+	public LibException(String msg) {super(msg);}
+
+}
+
---
+++ Exam20230717_Library/src/library/LibraryManager.java
@@ -0,0 +1,170 @@
+package library;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+
+
+public class LibraryManager {
+	    
+    // R1: Readers and Books 
+    
+    /**
+	 * adds a book to the library archive
+	 * The method can be invoked multiple times.
+	 * If a book with the same title is already present,
+	 * it increases the number of copies available for the book
+	 * 
+	 * @param title the title of the added book
+	 * @return the ID of the book added 
+	 */
+    public String addBook(String title) {
+        return null;
+    }
+    
+    /**
+	 * Returns the book titles available in the library
+	 * sorted alphabetically, each one linked to the
+	 * number of copies available for that title.
+	 * 
+	 * @return a map of the titles liked to the number of available copies
+	 */
+    public SortedMap<String, Integer> getTitles() {    	
+        return null;
+    }
+    
+    /**
+	 * Returns the books available in the library
+	 * 
+	 * @return a set of the titles liked to the number of available copies
+	 */
+    public Set<String> getBooks() {    	    	
+        return null;
+    }
+    
+    /**
+	 * Adds a new reader
+	 * 
+	 * @param name first name of the reader
+	 * @param surname last name of the reader
+	 */
+    public void addReader(String name, String surname) {
+    }
+    
+    
+    /**
+	 * Returns the reader name associated to a unique reader ID
+	 * 
+	 * @param readerID the unique reader ID
+	 * @return the reader name
+	 * @throws LibException if the readerID is not present in the archive
+	 */
+    public String getReaderName(String readerID) throws LibException {
+        return null;
+    }    
+    
+    
+    // R2: Rentals Management
+    
+    
+    /**
+	 * Retrieves the bookID of a copy of a book if available
+	 * 
+	 * @param bookTitle the title of the book
+	 * @return the unique book ID of a copy of the book or the message "Not available"
+	 * @throws LibException  an exception if the book is not present in the archive
+	 */
+    public String getAvailableBook(String bookTitle) throws LibException {
+        return null;
+    }   
+
+    /**
+	 * Starts a rental of a specific book copy for a specific reader
+	 * 
+	 * @param bookID the unique book ID of the book copy
+	 * @param readerID the unique reader ID of the reader
+	 * @param startingDate the starting date of the rental
+	 * @throws LibException  an exception if the book copy or the reader are not present in the archive,
+	 * if the reader is already renting a book, or if the book copy is already rented
+	 */
+	public void startRental(String bookID, String readerID, String startingDate) throws LibException {
+    }
+    
+	/**
+	 * Ends a rental of a specific book copy for a specific reader
+	 * 
+	 * @param bookID the unique book ID of the book copy
+	 * @param readerID the unique reader ID of the reader
+	 * @param endingDate the ending date of the rental
+	 * @throws LibException  an exception if the book copy or the reader are not present in the archive,
+	 * if the reader is not renting a book, or if the book copy is not rented
+	 */
+    public void endRental(String bookID, String readerID, String endingDate) throws LibException {
+    }
+    
+    
+   /**
+	* Retrieves the list of readers that rented a specific book.
+	* It takes a unique book ID as input, and returns the readers' reader IDs and the starting and ending dates of each rental
+	* 
+	* @param bookID the unique book ID of the book copy
+	* @return the map linking reader IDs with rentals starting and ending dates
+	* @throws LibException  an exception if the book copy or the reader are not present in the archive,
+	* if the reader is not renting a book, or if the book copy is not rented
+	*/
+    public SortedMap<String, String> getRentals(String bookID) throws LibException {
+        return null;
+    }
+    
+    
+    // R3: Book Donations
+    
+    /**
+	* Collects books donated to the library.
+	* 
+	* @param donatedTitles It takes in input book titles in the format "First title,Second title"
+	*/
+    public void receiveDonation(String donatedTitles) {
+    }
+    
+    // R4: Archive Management
+
+    /**
+	* Retrieves all the active rentals.
+	* 
+	* @return the map linking reader IDs with their active rentals
+
+	*/
+    public Map<String, String> getOngoingRentals() {
+        return null;
+    }
+    
+    /**
+	* Removes from the archives all book copies, independently of the title, that were never rented.
+	* 
+	*/
+    public void removeBooks() {
+    }
+    	
+    // R5: Stats
+    
+    /**
+	* Finds the reader with the highest number of rentals
+	* and returns their unique ID.
+	* 
+	* @return the uniqueID of the reader with the highest number of rentals
+	*/
+    public String findBookWorm() {
+        return null;
+    }
+    
+    /**
+	* Returns the total number of rentals by title. 
+	* 
+	* @return the map linking a title with the number of rentals
+	*/
+    public Map<String,Integer> rentalCounts() {
+        return null;
+    }
+
+}
