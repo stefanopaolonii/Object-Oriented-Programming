8f5f4c813e9bbd67f690253b29c1d974ee3ef589
Stefano <s283110@studenti.polito.it>
2024-08-21 17:08:45 +0200

    Initial commit

---
+++ Exam20230627_Med Center/src/med/MedException.java
@@ -0,0 +1,11 @@
+package med;
+
+public class MedException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public MedException() {super("Medical center exception");}
+	
+	public MedException(String msg) {super(msg);}
+
+}
